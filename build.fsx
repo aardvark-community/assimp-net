458984e99803ec485e798217098b3548a56b74ee
Georg Haaser <krauthaufen@awx.at>
2020-01-18 13:58:19 +0100

    updated build script

--- build.fsx
+++ build.fsx
@@ -1,10 +1,15 @@
-#load @"paket-files/build/vrvis/Aardvark.Fake/DefaultSetup.fsx"
 
-open Fake
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
+#load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
+
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Core
+open Fake.Tools
+open Fake.IO.Globbing.Operators
 
 //do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
@@ -14,5 +19,8 @@ DefaultSetup.install ["src/AssimpNet.sln"]
 #if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
 #endif
+
+
+
 
 entry()
